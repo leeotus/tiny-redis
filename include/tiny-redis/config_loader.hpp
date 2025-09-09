a05311de16c1ab91ccebdf87795f3add9c52f49a
leeotus <leeotus@163.com>
2025-09-10 00:19:16 +0800

    configurations loader

---
+++ include/tiny-redis/config_loader.hpp
@@ -0,0 +1,13 @@
+#ifndef __TINY_REDIS_CONFIG_LOADER_HPP__
+#define __TINY_REDIS_CONFIG_LOADER_HPP__
+
+#include <string>
+#include "config.hpp"
+
+namespace tiny_redis {
+
+    bool loadConfigFromFile(const std::string &path, ServerConfig &cfg, std::string &err);
+
+}  // namespace tiny_redis
+
+#endif
